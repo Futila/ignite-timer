7232cf566f8785ed77580c653ad537796ff9fb31
Fernando Futila <fernando.futila@gmail.com>
2024-03-21 23:33:01 -0300

    add style Layout, Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,22 @@
+import { NavLink } from "react-router-dom";
+import { Timer, Scroll } from "phosphor-react";
+import logoIgnite from "../../assets/logo.svg";
+import { HeaderContainer } from "./styles";
+
 export function Header() {
   return (
-    <header>
-      <h1>Header</h1>
-    </header>
+    <HeaderContainer>
+      <img src={logoIgnite} alt="" />
+
+      <nav>
+        <NavLink to="/" title="timer">
+          <Timer size={24} />
+        </NavLink>
+
+        <NavLink to="/history" title="Histórico">
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
+    </HeaderContainer>
   );
 }
